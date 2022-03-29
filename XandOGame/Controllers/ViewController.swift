9e14d0d06003b293dae9f50e6e62acdc574b8ada
Korobskoy Roman <korobskoyra@gmail.com>
2022-03-29 22:03:46 +0300

    crash fix, added error label while username is empty

--- XandOGame/Controllers/ViewController.swift
+++ XandOGame/Controllers/ViewController.swift
@@ -16,21 +16,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupUI()
-    }
-
-    func setupUI() {
-        startButton.layer.cornerRadius = 10
-        alertView.layer.cornerRadius = 10
-        alertView.layer.shadowOpacity = 0.4
-        alertView.layer.shadowColor = UIColor.black.cgColor
-        alertView.layer.shadowRadius = 10
-        alertView.layer.shadowOffset = .zero
     }
     
     @IBAction func startButtonClicked(_ sender: UIButton) {
-        guard !nameField.text!.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        guard !nameField.text!.trimmingCharacters(in: .whitespaces).isEmpty else {
+            nameErrorLabel.isHidden = false
+            return
+        }
         
         let controller = storyboard?.instantiateViewController(withIdentifier: "gameVC") as! GameViewController
         controller.playerName = nameField.text
