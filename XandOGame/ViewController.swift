d7d53c77f3a6590d6b834b1265f1deb04b4de292
Korobskoy Roman <korobskoyra@gmail.com>
2022-03-29 20:09:05 +0300

    initial commit

--- XandOGame/ViewController.swift
+++ XandOGame/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var alertView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
 
-
+    func setupUI() {
+        startButton.layer.cornerRadius = 10
+        alertView.layer.cornerRadius = 10
+        alertView.layer.shadowOpacity = 0.4
+        alertView.layer.shadowColor = UIColor.black.cgColor
+        alertView.layer.shadowRadius = 10
+        alertView.layer.shadowOffset = .zero
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameField.resignFirstResponder()
+    }
 }
 
